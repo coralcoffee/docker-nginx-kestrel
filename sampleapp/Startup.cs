dc8753714f271c27fb122b093830cd8cc9ecec24
Gary Yin <ygchina@hotmail.com>
2020-06-04 19:37:44 -0600

    dd

--- sampleapp/Startup.cs
+++ sampleapp/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace sampleapp
 {
@@ -29,8 +30,6 @@ namespace sampleapp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // using Microsoft.AspNetCore.HttpOverrides;
-
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
