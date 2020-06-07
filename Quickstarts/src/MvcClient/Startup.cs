5936131883b2639eca20b09df39211513461d24e
Gary Yin <ygchina@hotmail.com>
2020-06-07 11:18:05 -0600

    eeeee

--- Quickstarts/src/MvcClient/Startup.cs
+++ Quickstarts/src/MvcClient/Startup.cs
@@ -24,7 +24,7 @@ namespace MvcClient
                 .AddCookie("Cookies")
                 .AddOpenIdConnect("oidc", options =>
                 {
-                    options.Authority = "https://ubuntu-dev.southcentralus.cloudapp.azure.com/";
+                    options.Authority = "https://ubuntu-dev.southcentralus.cloudapp.azure.com/account/login";
                     //options.RequireHttpsMetadata = false;
                     options.CallbackPath = new PathString("/sample/signin-oidc");
                     options.ClientId = "mvc";
