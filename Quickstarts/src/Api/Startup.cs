4998246f2689d712424c35d7cc74588222113441
Gary Yin <ygchina@hotmail.com>
2020-06-07 00:58:02 -0600

    ddddddddddddd

--- Quickstarts/src/Api/Startup.cs
+++ Quickstarts/src/Api/Startup.cs
@@ -16,7 +16,7 @@ namespace Api
             services.AddAuthentication("Bearer")
                 .AddJwtBearer("Bearer", options =>
                 {
-                    options.Authority = "https://ubuntu-dev.southcentralus.cloudapp.azure.com/";
+                    options.Authority = "https://idsrv.local/";
                     options.RequireHttpsMetadata = false;
 
                     options.Audience = "api1";
